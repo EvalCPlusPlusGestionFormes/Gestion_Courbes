eee59dcc4dba6f1649dc16a720a8422e2f343e1b
Antw1 <antoine-corbin@live.fr>
2019-01-17 08:53:15 +0100

    BETA V.1.2 (Modification du logo)

--- gestion/main.cpp
+++ gestion/main.cpp
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
     creation = new ctrlcreation();
 
     //Ajout du logo pour l'application
-    QApplication::setWindowIcon(QIcon(":/vue/Logo_Eval.jpg"));
+    QApplication::setWindowIcon(QIcon(":/vue/logo_Eval.png"));
 
 
    r = a.exec();
